0fe5187c7dc38ae46f33985a5873c408b2c9ae92
Elvira <elvira.mammadova03@gmail.com>
2021-06-11 13:47:40 +0200

    Added more hello feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -4,7 +4,7 @@ public class HelloTest {
 
         System.out.println("HelloTest");
         System.out.println("Developing Hello feature");
-
-
+        System.out.println("Local Repo is already published to GitHub");
+        System.out.println("Doing more work");
     }
 }
